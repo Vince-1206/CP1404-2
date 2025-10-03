d7acfa4e7eba76c78dcf0b2d14cc7c2447ac1a9c
Vince <vincelin126@gmail.com>
2025-10-03 16:34:20 +0800

    Task 3 correct

--- Week 3/HomeWork/capitalist_conrad.py
+++ Week 3/HomeWork/capitalist_conrad.py
@@ -7,42 +7,26 @@ a 50% chance that it decreases by 0 to 5%.
 If the price rises above $1000, or falls below $0.01, the program should end.
 The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
 """
+
 import random
 
 FILENAME = "output.txt"
-MAX_INCREASE = 0.175  # 17.5%
-MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 1.0
-MAX_PRICE = 100.0
+MAX_INCREASE = 0.10     # 10%
+MAX_DECREASE = 0.05     # 5%
+MIN_PRICE = 0.01
+MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 
 price = INITIAL_PRICE
-number_of_days = 0
+day = 0
 
-# Open file for writing
-out_file = open(FILENAME, 'w')
-
-# Initial output
-print(f"Starting price: ${price:,.2f}", file=out_file)
-
-
-while MIN_PRICE <= price <= MAX_PRICE:
-    price_change = 0
-    # generate a random integer of 1 or 2
-    # if it's 1, the price increases, otherwise it decreases
-    if random.randint(1, 2) == 1:
-        # generate a random floating-point number
-        # between 0 and MAX_INCREASE
-        price_change = random.uniform(0, MAX_INCREASE)
-    else:
-        # generate a random floating-point number
-        # between negative MAX_DECREASE and 0
-        price_change = random.uniform(-MAX_DECREASE, 0)
-
-    price *= (1 + price_change)
-    number_of_days += 1
-    # Output for the current day
-    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
-
-# Close file
-out_file.close()
+with open(FILENAME, "w") as out_file:
+    print(f"Starting price: ${price:,.2f}", file=out_file)
+    while MIN_PRICE <= price <= MAX_PRICE:
+        if random.randint(1, 2) == 1:
+            change = random.uniform(0, MAX_INCREASE)
+        else:
+            change = random.uniform(-MAX_DECREASE, 0)
+        price *= (1 + change)
+        day += 1
+        print(f"On day {day} price is: ${price:,.2f}", file=out_file)
