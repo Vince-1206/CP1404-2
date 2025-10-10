cc01df48d40b5e0342c1f446482721f649ae2cfa
Vince <vincelin126@gmail.com>
2025-10-10 16:00:17 +0800

    Practical 4

--- Week 4/Week 4 Practical.py
+++ Week 4/Week 4 Practical.py
@@ -1,3 +1,4 @@
+"""
 # List of names
 names = ["Ada", "Alan", "Bill", "John"]
 
@@ -7,10 +8,8 @@ print("Current names:", ", ".join(names))
 while True:
     name_to_remove = input("Who do you want to remove? (Press Enter to stop) ").strip()
 
-
     if name_to_remove == "":
         break
-
 
     if name_to_remove in names:
         names.remove(name_to_remove)
@@ -21,3 +20,24 @@ while True:
     print("Current names:", ", ".join(names))
 
 print("\nFinal list of names:", ", ".join(names))
+
+"""
+"""
+values = [1, 2, 3, 2]
+values.remove(2)
+print(values)
+"""
+"""
+before = [1, 4, 0, -1]
+after = before.sort()
+print(after[0])
+"""
+
+"""
+words = ["aye", "bee", "sea"]
+print("/".join(words))
+"""
+
+words = ["aye", "bee", "sea", "bee"]
+words.remove("bee")
+print(words.pop())
