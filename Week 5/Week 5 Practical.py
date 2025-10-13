5fd2370390b812a8126bb43320971304e32bb499
Vince <vincelin126@gmail.com>
2025-10-13 13:09:11 +0800

    Practical 5 practice

--- Week 5/Week 5 Practical.py
+++ Week 5/Week 5 Practical.py
@@ -1,8 +1,7 @@
 data = [['Derek', 7], ['Xavier', 80], ['Bob', 612], ['Chantanelle', 9]]
 
-# Find the longest name length (for alignment)
+
 max_len = max(len(name) for name, score in data)
 
-# Print each name and score neatly aligned
 for name, score in data:
     print(f"{name:<{max_len}} = {score}")
