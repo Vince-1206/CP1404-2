d4c41275c2a7ffeb255afcc1b3961f6d30b1b994
Vince <vincelin126@gmail.com>
2025-10-13 13:12:51 +0800

    Practical 5 practice

--- Week 5/Week 5 Practical.py
+++ Week 5/Week 5 Practical.py
@@ -1,7 +1,8 @@
 data = [['Derek', 7], ['Xavier', 80], ['Bob', 612], ['Chantanelle', 9]]
 
+sorted_data = sorted(data, key=lambda item: item[1], reverse=True)
 
-max_len = max(len(name) for name, score in data)
+max_len = max(len(name) for name, score in sorted_data)
 
-for name, score in data:
+for name, score in sorted_data:
     print(f"{name:<{max_len}} = {score}")
