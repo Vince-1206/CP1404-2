f3fbfb47e9d41a640b5ac7949e7305b3e8aab6ec
Vince <vincelin126@gmail.com>
2025-10-13 13:31:12 +0800

    Practical 5 practice

--- Week 5/Week 5 Practical.py
+++ Week 5/Week 5 Practical.py
@@ -10,10 +10,12 @@
 #     name, score = pair
 #     print(f"{name:{name_width}} = {score:{score_width}}")
 
+from operator import itemgetter
 data = [['Derek', 7], ['Xavier', 80], ['Bob', 612], ['Chantanelle', 9]]
 name_width = max((len(pair[0]) for pair in data))
 score_width = max((len(str(pair[1])) for pair in data))
 
-for pair in data:
+
+for pair in sorted(data, key=itemgetter(1), reverse=True):
     name, score = pair
     print(f"{name:{name_width}} = {score:{score_width}}")
