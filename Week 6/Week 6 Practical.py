94ee085a4bb3714f4230166395eeb7ecac5723c6
Vince <vincelin126@gmail.com>
2025-10-27 14:51:49 +0800

    practical 6 class practice

--- Week 6/Week 6 Practical.py
+++ Week 6/Week 6 Practical.py
@@ -12,3 +12,22 @@ print(scary_monsters)
 
 """
 
+class Monster:
+    def __int__ (self, name, scariness, color):
+        self.name = name
+        self.scariness = scariness
+        self.color = color
+
+    def is_scary(self):
+        return self.scariness > 100  # You can adjust this threshold
+
+monsters = [
+    Monster("Mike", 340, "blue"),
+    Monster("James", 14, "green"),
+    Monster("Randall", 24, "purple")
+]
+
+scary_monsters = [monster for monster in monsters if monster.is_scary()]
+
+for m in scary_monsters:
+    print(m.name)
